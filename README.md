2cfb0d9513f7ce7fa8399c4f1954786f9467a850
mattdunn <contact@mattonsoftware.com>
2012-02-14 22:24:24 +0000

    added sections and started character classes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-  * Finding regex to be a write once, read zero medium? Dont see whats regular about regex?
+Finding regex to be a write once, read zero medium? Dont see whats regular about regex? 
 
-  * Learn to use regex like a pro:
+Learn to use regex like a pro:
 
   ![YO DAWG, I HERD YOU LIKE MATCHING, SO I PUT A REGEX IN YOUR CODE SO YOU CAN MATCH WHILE YOU MATCH](https://github.com/mattdunn/learning-regex/raw/master/lib/xzibit-regex-meme.jpg)
 
--- learnings/starting_out.rb
+++ learnings/starting_out.rb
@@ -1,17 +1,25 @@
-# literal text, matching anywhere
+# literal text
+# matching anywhere
 'Elvis, the king, rocks'.should_match 'the king'
 'Elvis, the king, rocks'.shouldnt_match 'bling'
 
+
+# starting or ending with: ^ $
 # ^ starting with
 'Elvis, the king, rocks'.should_match '^Elvis'
 'Elvis, the king, rocks'.shouldnt_match '^king'
-
 # $ ending with
 'Elvis, the king, rocks'.should_match 'rocks$'
 'Elvis, the king, rocks'.shouldnt_match 'king$'
 
-# [] any of
+
+# character class: [] 
+# [] any of, matching a single character (exactly matching case)
 'Elvis, the king, rocks'.should_match 'r[o]cks'
 'Elvis, the king, rocks'.should_match 'r[oa]cks'
 'Elvis, the king, racks'.should_match 'r[oa]cks'
 'Elvis, the king, rocks'.shouldnt_match 'r[i]cks'
+
+# [] any of, matching a single character (exactly matching case), within a range
+'Elvis, the king, rocks 4 eva'.should_match '[1-5]'
+'Elvis, the king, rocks 6 eva'.shouldnt_match '[1-5]'
