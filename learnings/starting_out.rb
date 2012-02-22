6e75bcf53890f896d3da65371e71c4f5dc7b4a22
mattdunn <contact@mattonsoftware.com>
2012-02-22 22:33:46 +0000

    more character class examples

--- learnings/starting_out.rb
+++ learnings/starting_out.rb
@@ -14,12 +14,24 @@
 
 
 # character class: [] 
-# [] any of, matching a single character (exactly matching case)
+# [a] any of, matching a single character (exactly matching case)
 'Elvis, the king, rocks'.should_match 'r[o]cks'
 'Elvis, the king, rocks'.should_match 'r[oa]cks'
 'Elvis, the king, racks'.should_match 'r[oa]cks'
 'Elvis, the king, rocks'.shouldnt_match 'r[i]cks'
 
-# [] any of, matching a single character (exactly matching case), within a range
+# [a-b] any of, matching a single character (exactly matching case), within a range
 'Elvis, the king, rocks 4 eva'.should_match '[1-5]'
 'Elvis, the king, rocks 6 eva'.shouldnt_match '[1-5]'
+
+# [a-b1-2] any of, matching a single character (exactly matching case), within any of the given ranges in any order
+'Elvis, the king, rocks 4 eva'.should_match '[1-5A-E]'
+'Elvis, the king, rocks 4 eva'.should_match '[1-5w-z]'
+'Elvis, the king, rocks 4 eva'.shouldnt_match '[5-9y-z]'
+
+# [-a] literal text match, as dash as first character in character class is not considered to denote a range
+'Elvis, the king, rocks 4-eva'.should_match '[-]'
+'Elvis, the king, rocks 4 eva'.shouldnt_match '[-]'
+
+# [^a-b] negated range, doesn't match any of, matching a single character (exactly matching case), within a range
+'Elvis, the king, rocks 4 eva'.should_match '[^y-z]'
