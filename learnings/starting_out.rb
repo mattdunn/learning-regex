8ed6957dcb34b99fe95b6643066051a3fa9fd70a
mattdunn <contact@mattonsoftware.com>
2012-03-12 08:04:02 +0000

    constrained alternative example

--- learnings/starting_out.rb
+++ learnings/starting_out.rb
@@ -42,3 +42,8 @@
 # [|] matches any of provided expressions (alternatives)
 'Elvis, the king, rocks 4 eva'.should_match '[Elvis|not_found]'
 #'Elvis, the king, rocks 4 eva'.shouldnt_match '[notfound]'  Why doesnt this fail?
+
+# [|] matches any of provided constrainted expressions (alternatives)
+'Elvis, the king, rocks 4 eva'.should_match '[k(i|o)ng]'
+'Elvis, the king, rocks 4 eva'.should_match '[k(x|y)ng]'
+
