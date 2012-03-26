17a5e14013954f82fec846b7ef2671b6750102e6
mattdunn <contact@mattonsoftware.com>
2012-03-26 07:47:27 +0100

    added repetition, optional tests

--- learnings/05-alternatives.rb
+++ learnings/05-alternatives.rb
@@ -2,6 +2,6 @@
 'Elvis, the king, rocks 4 eva'.should_match '[Elvis|not_found]'
 #'Elvis, the king, rocks 4 eva'.shouldnt_match '[notfound]'  #Why doesnt this fail?
 
-# [|] matches any of provided constrained expressions (alternatives)
+# (|) matches any of provided constrained expressions (alternatives)
 'Elvis, the king, rocks 4 eva'.should_match 'k(i|o)ng'
 'Elvis, the king, rocks 4 eva'.shouldnt_match 'k(x|y)ng'
